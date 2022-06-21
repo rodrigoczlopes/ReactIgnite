5f90597a8166a4d59e23e783a3a3a13a72c99c02
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-06-21 15:22:49 -0300

    Atualização do dia 21 06

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ function App() {
 
   return (
     <div>
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
+    <Post 
+      author="Rodrigo Lopes"
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore optio sint quidem, voluptatum quo corrupti, dolore aliquam distinctio fugit architecto odio illo consequuntur iusto perferendis sequi at quaerat nisi?"
+    />
     </div>
     
   )
