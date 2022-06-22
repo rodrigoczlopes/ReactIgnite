6b0c3341a08312416f30c5b15591939b55e0bd32
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-06-22 17:10:07 -0300

    inicio do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Header } from './components/Header';
 import {Post} from './Post';
+import './global.css';
 
 function App() {
  
 
   return (
     <div>
+    <Header />
+
     <Post 
       author="Rodrigo Lopes"
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore optio sint quidem, voluptatum quo corrupti, dolore aliquam distinctio fugit architecto odio illo consequuntur iusto perferendis sequi at quaerat nisi?"
+    />
+
+    <Post 
+      author="Juliete Figueiredo"
       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore optio sint quidem, voluptatum quo corrupti, dolore aliquam distinctio fugit architecto odio illo consequuntur iusto perferendis sequi at quaerat nisi?"
     />
     </div>
