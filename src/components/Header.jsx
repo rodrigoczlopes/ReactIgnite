94cc9873c988c1f2d2652c877d847ffcbb25ae1b
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-07-12 10:55:12 -0300

    css alterado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import styles from './Header.module.css'
 import igniteLogo from '../assets/ignite-logo.svg';
+import reactLogo from '../assets/react-icon.svg';
 
 export function Header(){
   return(
     <header className={styles.header}>
 
-      <img src={igniteLogo} alt="Logotipo do Ignite" />
+      <img src={reactLogo} alt="Logotipo do Ignite" />
 
-    <strong>Ignite Feed</strong>
+    <strong>Ignite Curse</strong>
 
 
     </header>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,11 +5,14 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img 
         className={styles.cover} 
-        src="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+        src="https://images.unsplash.com/photo-1500043357865-c6b8827edf10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+        
       />
     
 
     <div className={styles.profile}>
+      <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/52975315?v=4" />
+
       <strong>Rodrigo Lopes</strong>
       <span>Web Developer</span>
     </div>
