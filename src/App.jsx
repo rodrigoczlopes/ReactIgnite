421bcb7f4ce7d917b0e3784fbf022cc4b43c23b7
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-07-14 14:56:10 -0300

    post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Header } from './components/Header';
 import {Post} from './components/Post';
-import './global.css';
-import styles from './App.module.css';
 import {Sidebar} from './components/Sidebar';
+
+import styles from './App.module.css';
+import './global.css';
 
 function App() {
  
