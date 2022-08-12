577357ba3999836cfff292a11b13cf3d4cbfcebb
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-08-12 17:27:15 -0300

    alteração1

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img 
         className={styles.cover} 
-        src="https://images.unsplash.com/photo-1500043357865-c6b8827edf10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+        src="https://images.unsplash.com/photo-1660233217113-62b97fedcbda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
         
       />
     
