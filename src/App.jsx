04b69b417fd488080d453ad010fbaac8742ebf68
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-08-16 15:37:55 -0300

    começo etapa3

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,42 @@ import {Sidebar} from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
-function App() {
+const posts = [
+  {
+    id: 1,
+    author:{
+      avatarUrl:'http://github.com.br/rodrigoczlopes.png',
+      name: 'Rodrigo Lopes',
+      role: 'Developer'
+    },
+    content:[       
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: ' Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content:'jane.design/doctorcare'},
+      
+    ],
+    publishedAt: new Date('2022-08-10 20:00:00'),
+  },
+  {
+    id: 2,
+    author:{
+      avatarUrl:'http://github.com.br/rodrigoczlopes.png',
+      name: 'Juliete Figueiredo',
+      role: 'Educator'
+    },
+    content:[       
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: ' Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content:'jane.design/doctorcare'},
+      
+    ], 
+    publishedAt: new Date('2022-08-16 20:00:00'),
+  },
+]
+
+
+
+export function App() {
  
 
   return (
